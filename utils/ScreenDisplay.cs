5e12cfaa00db0fbb5c5d5d1a6d7793c044353852
Grld-Andy <andyansong82@gmail.com>
2026-02-24 14:32:36 +0000

    style: change header color

--- utils/ScreenDisplay.cs
+++ utils/ScreenDisplay.cs
@@ -4,8 +4,10 @@ public static class ScreenDisplay
 {
     public static void ShowTitle(string title)
     {
-        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Blue;
         // Thread.Sleep(500);
+        Console.Clear();
         Console.WriteLine(title.ToUpper());
+        Console.ForegroundColor = ConsoleColor.White;
     }
 }
