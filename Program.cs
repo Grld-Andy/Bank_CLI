db99284ff5c811a27b9c9a442dd8bbe20d4dbfb2
Grld-Andy <andyansong82@gmail.com>
2026-02-24 10:58:17 +0000

    feat: create homescreen

--- Program.cs
+++ Program.cs
@@ -86,6 +86,29 @@ public class Program
         System.Console.WriteLine($"{name} logged in successfully");
     }
 
+    // home screen
+    public static void HomeScreen()
+    {
+        int choice;
+        string? choiceString;
+        do
+        {
+            System.Console.WriteLine("Welcome, what would you like to do?\n Please enter a number\n\n1. Withdraw\n2. Deposit\n3. View Balance\n4. Logout");
+            choiceString = Console.ReadLine();
+        }while(int.TryParse(choiceString, out choice));
+
+        switch (choice)
+        {
+            case 1:
+                System.Console.WriteLine("hello");
+                break;
+            default:
+                System.Console.WriteLine("Sorry, please try again");
+                HomeScreen();
+                break;
+        }
+    }
+
     // withdraw
 
     // deposit
